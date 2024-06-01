c8a21aea92508315fb1798c124a2264be3b03b24
Patrick Singer <patrick.singer1999@gmail.com>
2024-06-01 18:39:52 +0200

    renaming

--- activate_speaker.bat
+++ activate_speaker.bat
@@ -3,9 +3,9 @@
 :: Switch to audio device
 nircmd.exe setdefaultsounddevice "Speakers" 1
 
-nircmd.exe setsysvolume 32000
+nircmd.exe setsysvolume 64000
 
 nircmd.exe mediaplay 1000 "C:\Windows\Media\Windows Balloon.wav"
 
-nircmd.exe setsysvolume 16000
+nircmd.exe setsysvolume 32000
 
