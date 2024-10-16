b6349f7393fa53e270a5be68dd50bbbdae0cd595
Patrick Singer <patrick.singer1999@gmail.com>
2024-10-16 12:16:31 +0200

    Update activate_tv.bat

--- activate_tv.bat
+++ activate_tv.bat
@@ -2,4 +2,5 @@
 
 :: Switch to audio device
 nircmd.exe setdefaultsounddevice "LG TV SSCR2" 1
+nircmd.exe setdefaultsounddevice "TV" 1
 nircmd.exe mediaplay 1000 "C:\Windows\Media\Windows Balloon.wav"
